85d67131c41489cf056b08febf9cda014b954ec6
DEVIKA A <bijudevika881@gmail.com>
2026-03-10 11:54:15 +0530

    Refactor SRTF scheduling algorithm for clarity

--- srtf.c
+++ srtf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 
 struct process {
     char pid[10];
@@ -9,62 +10,86 @@ struct process {
 int main() {
     int n;
     scanf("%d", &n);
-
+    
     struct process p[20];
-
+    
     for(int i = 0; i < n; i++) {
         scanf("%s %d %d", p[i].pid, &p[i].at, &p[i].bt);
         p[i].rt = p[i].bt;
     }
-
-    int completed = 0, time = 0, min_index;
-    int min_rt;
-
+    
+    int completed = 0;
+    int current_time = 0;
+    int total_wt = 0, total_tat = 0;
+    int min_rt_index;
+    int shortest = -1;
+    
+    // Array to track if process is completed
+    int is_completed[20] = {0};
+    
     while(completed < n) {
-
-        min_rt = 9999;
-        min_index = -1;
-
+        // Find process with minimum remaining time among arrived processes
+        shortest = -1;
+        int min_rt = INT_MAX;
+        
         for(int i = 0; i < n; i++) {
-            if(p[i].at <= time && p[i].rt > 0 && p[i].rt < min_rt) {
-                min_rt = p[i].rt;
-                min_index = i;
+            if(p[i].at <= current_time && !is_completed[i]) {
+                if(p[i].rt < min_rt) {
+                    min_rt = p[i].rt;
+                    shortest = i;
+                }
+                // If remaining time is same, choose the one that arrived earlier
+                else if(p[i].rt == min_rt && p[i].at < p[shortest].at) {
+                    shortest = i;
+                }
             }
         }
-
-        if(min_index == -1) {
-            time++;
+        
+        if(shortest == -1) {
+            // If no process has arrived yet, increment time
+            current_time++;
             continue;
         }
-
-        p[min_index].rt--;
-        time++;
-
-        if(p[min_index].rt == 0) {
+        
+        // Execute the selected process for 1 time unit
+        p[shortest].rt--;
+        current_time++;
+        
+        // If process is completed
+        if(p[shortest].rt == 0) {
             completed++;
-
-            p[min_index].ct = time;
-            p[min_index].tat = p[min_index].ct - p[min_index].at;
-            p[min_index].wt = p[min_index].tat - p[min_index].bt;
+            is_completed[shortest] = 1;
+            
+            // Calculate completion time
+            p[shortest].ct = current_time;
+            
+            // Calculate turnaround time
+            p[shortest].tat = p[shortest].ct - p[shortest].at;
+            
+            // Calculate waiting time
+            p[shortest].wt = p[shortest].tat - p[shortest].bt;
+            
+            // Add to totals
+            total_wt += p[shortest].wt;
+            total_tat += p[shortest].tat;
         }
     }
-
-    float avg_wt = 0, avg_tat = 0;
-
+    
+    // Print Waiting Time
     printf("Waiting Time:\n");
     for(int i = 0; i < n; i++) {
         printf("%s %d\n", p[i].pid, p[i].wt);
-        avg_wt += p[i].wt;
     }
-
+    
+    // Print Turnaround Time
     printf("Turnaround Time:\n");
     for(int i = 0; i < n; i++) {
         printf("%s %d\n", p[i].pid, p[i].tat);
-        avg_tat += p[i].tat;
     }
-
-    printf("Average Waiting Time: %.1f\n", avg_wt/n);
-    printf("Average Turnaround Time: %.1f\n", avg_tat/n);
-
+    
+    // Print Averages
+    printf("Average Waiting Time: %.1f\n", (float)total_wt/n);
+    printf("Average Turnaround Time: %.1f\n", (float)total_tat/n);
+    
     return 0;
 }
