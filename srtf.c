e4f00f6e36ef64becf97bae5815bd8a5e126a1d3
DEVIKA A <bijudevika881@gmail.com>
2026-03-10 11:47:55 +0530

    Add SRTF scheduling algorithm implementation
    Implemented Shortest Remaining Time First (SRTF) scheduling algorithm to calculate waiting and turnaround times for processes.

--- srtf.c
+++ srtf.c
@@ -17,14 +17,54 @@ int main() {
         p[i].rt = p[i].bt;
     }
 
-    ..
-      .
-      ..
-      .
-      ..
-      .
-      ..
-      
+    int completed = 0, time = 0, min_index = -1;
+    int min_rt;
+
+    while(completed < n) {
+
+        min_rt = 9999;
+        min_index = -1;
+
+        for(int i = 0; i < n; i++) {
+            if(p[i].at <= time && p[i].rt > 0 && p[i].rt < min_rt) {
+                min_rt = p[i].rt;
+                min_index = i;
+            }
+        }
+
+        if(min_index == -1) {
+            time++;
+            continue;
+        }
+
+        p[min_index].rt--;
+        time++;
+
+        if(p[min_index].rt == 0) {
+            completed++;
+
+            p[min_index].ct = time;
+            p[min_index].tat = p[min_index].ct - p[min_index].at;
+            p[min_index].wt = p[min_index].tat - p[min_index].bt;
+
+            if(p[min_index].wt < 0)
+                p[min_index].wt = 0;
+        }
+    }
+
+    float avg_wt = 0, avg_tat = 0;
+
+    printf("Waiting Time:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%s %d\n", p[i].pid, p[i].wt);
+        avg_wt += p[i].wt;
+    }
+
+    printf("Turnaround Time:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%s %d\n", p[i].pid, p[i].tat);
+        avg_tat += p[i].tat;
+    }
 
     printf("Average Waiting Time: %.1f\n", avg_wt/n);
     printf("Average Turnaround Time: %.1f\n", avg_tat/n);
